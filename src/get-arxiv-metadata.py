f911b3bac41f4fedc9aa967a54935130b5a683c0
Naoto Shioya <shioya.naoto17@gmail.com>
2023-11-07 09:15:05 +0900

    add fetch category logic

--- src/get-arxiv-metadata.py
+++ src/get-arxiv-metadata.py
@@ -14,16 +14,16 @@ results = client.results(search)
 all_results = list(results)
 
 pprint.pprint(all_results)
-# # all_resultsを辞書型に変換して、キーを'title', 'summary', 'published'に絞る
-# all_results = [vars(s) for s in all_results]
-# all_results = [{k: v for k, v in d.items() if k in ('title', 'summary', 'published', 'entry_id')} for d in all_results]
-# # publishedをdatetime型に変換する
-# for d in all_results:
-#   d['published'] = d['published'].strftime('%Y-%m-%d')
+# all_resultsを辞書型に変換して、キーを'title', 'summary', 'published'に絞る
+all_results = [vars(s) for s in all_results]
+all_results = [{k: v for k, v in d.items() if k in ('title', 'summary', 'published', 'entry_id', 'primary_category')} for d in all_results]
+# publishedをdatetime型に変換する
+for d in all_results:
+  d['published'] = d['published'].strftime('%Y-%m-%d')
 
-# pprint.pprint(all_results)
+pprint.pprint(all_results)
 
-# # all_resultsをjson形式で保存する
-# import json
-# with open('data.json', 'w') as f:
-#   json.dump(all_results, f, indent=2)
+# all_resultsをjson形式で保存する
+import json
+with open('data.json', 'w') as f:
+  json.dump(all_results, f, indent=2)
